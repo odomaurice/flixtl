c6a35eace8762eedfe890ca85c6e3244f3931b94
Odo Maurice <odomaurice@yahoo.com>
2024-02-25 10:35:30 +0100

    updated the utils.js mongoDB integration

--- src/app/lib/utils.js
+++ src/app/lib/utils.js
@@ -1,35 +1,15 @@
 import mongoose from "mongoose";
 
-const mongoAtlasUri =
-  "mongodb+srv://apex:SD8DUA3bs6e2OeRD@cluster0.zatn07o.mongodb.net/apex?retryWrites=true&w=majority";
 
-try {
-  // Connect to the MongoDB cluster
-  mongoose.connect(
-    mongoAtlasUri,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    () => console.log(" Mongoose is connected"),
-  );
-} catch (e) {
-  console.log("could not connect");
-}
+const connection = {};
 
-const dbConnection = mongoose.connection;
-dbConnection.on("error", (err) => console.log(`Connection error ${err}`));
-dbConnection.once("open", () => console.log("Connected to DB!"));
-
-
-
-
-// const connection = {};
-
-// export const connectToDB = async () => {
-//   try {
-//     if (connection.isConnected) return;
-//     const db = await mongoose.connect(process.env.MONGODB_URI);
-//     connection.isConnected = db.connections[0].readyState;
-//   } catch (error) {
-//     console.log(error)
-//     throw new Error(error);
-//   }
-// };
+export const connectToDB = async () => {
+  try {
+    if (connection.isConnected) return;
+    const db = await mongoose.connect(process.env.MONGODB_URI);
+    connection.isConnected = db.connections[0].readyState;
+  } catch (error) {
+    console.log(error)
+    throw new Error(error);
+  }
+};
