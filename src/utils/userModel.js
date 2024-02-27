1b76ec9c377d1eb4e32b951eb8456b99e19775c5
Odo Maurice <odomaurice@yahoo.com>
2024-02-27 07:43:47 +0100

    model update

--- src/utils/userModel.js
+++ src/utils/userModel.js
@@ -15,12 +15,5 @@ const user = new mongoose.Schema(
   { timestamps: true }
 );
 
-let userModel;
 
-try {
-  userModel = mongoose.model("User");
-} catch (e) {
-  userModel = mongoose.model("User", user);
-}
 
-export default userModel;
